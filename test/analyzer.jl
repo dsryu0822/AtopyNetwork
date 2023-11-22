4093037db3b984817b1d841e9e21bfa70f62b5df
dsryu0822 <rmsmsgood@naver.com>
2023-11-22 17:08:51 +0900

    backup

---
+++ test/analyzer.jl
@@ -0,0 +1,86 @@
+using ProgressBars
+packages = [:JLD2, :SparseArrays, :DataFrames, :Plots, :Random]
+for package in ProgressBar(packages)
+    @eval using $(package)
+end
+
+struct WordNetwork
+    adj::AbstractSparseMatrix
+    nodes::Vector{String}
+end
+function Base.show(io::IO, WN::WordNetwork)
+    print(io, "$(length(WN.nodes))-node WordNetwork: ")
+    show(io, WN.nodes)
+end
+
+function neighbor(WN::WordNetwork, id_query::Integer)
+    if !(1 ≤ id_query ≤ length(WN.nodes))
+        error(BoundsError, ": node with id '$id_query' is not in the network")
+    end
+
+    id = findall(.!iszero.(WN.adj[id_query, :]))
+    node = WN.nodes[id]
+    weight = WN.adj[id_query, id]
+
+    df = DataFrame(; id, node, weight)
+    sort!(df, :weight, rev = true)
+    return df
+end
+function neighbor(WN::WordNetwork, query::String)
+    id_query = findfirst(WN.nodes .== query)
+    if id_query |> isnothing
+        error(KeyError, ": node with name '$query' is not in the network")
+    end
+    return neighbor(WN, id_query)
+end
+
+function subgraph(WN::WordNetwork, id, depth = 5, distance = 1)
+    if id isa String
+        id = findfirst(WN.nodes .== id)
+    end
+
+    df  = DataFrame(depth = 0, src = 0, id = id, weight = 0)
+    for i in 1:distance
+        for j in df[df.depth .== (i-1), :id]
+            if j in df.src continue end
+            df = [df; [
+                DataFrame(depth = fill(i, depth), src = j) neighbor(WN, j)[1:depth, [:id, :weight]]
+                ]]
+        end
+    end
+    rename!(df, :id => :dst)
+    df = df[df.src .!= df.dst, :]
+    # unique!(df, :dst)
+    return df
+end
+
+function plot_subgraph(WN::WordNetwork, id, depth = 5, distance = 2)
+    distance = distance + 2
+    SG = subgraph(WN, id, depth, distance)
+    _id = setdiff(unique([SG.src; SG.dst]), 0)
+    _node = WN.nodes[_id]
+    sn = length(_id)
+    
+    dict_id = Dict(_id .=> 1:sn)
+    
+    θ = shuffle(LinRange(0, 2π, sn) + 0.01randn(sn))
+    r = SG.depth[[findfirst(SG.dst .== id) for id in _id]]
+    x_ = r .* cos.(θ)
+    y_ = r .* sin.(θ)
+    
+    SG.src[1] = first(SG.dst)
+    srcx = x_[replace(SG.src, dict_id...)]
+    srcy = y_[replace(SG.src, dict_id...)]
+    dstx = x_[replace(SG.dst, dict_id...)]
+    dsty = y_[replace(SG.dst, dict_id...)]
+    
+    p1 = plot()
+    for mat = eachrow(stack([srcx, dstx, srcy, dsty]))
+        plot!(p1, mat[1:2], mat[3:4], color = :black, lw = 0.5, alpha = 0.5)
+    end
+    p1 = scatter!(x_, y_, text = _node,
+        color = :white, msw = 0, ms = 20, size = (900, 900), legend = :none, alpha = 0.8,
+        lims = (-distance+1, distance-1), framestyle=:none)
+
+    return p1
+end
--- test/corpus.jl
+++ test/corpus.jl
@@ -1,51 +1,81 @@
-using ProgressBars
-packages = [:TextAnalysis, :DataFrames, :Plots, :CSV, :Snowball]
+include("analyzer.jl")
+packages = [:TextAnalysis, :Snowball, :CSV]
 for package in ProgressBar(packages)
     @eval using $(package)
 end
-stopwords = CSV.read("stopwords.csv", DataFrame)
-stopwords = stopwords.voca[ismissing.(stopwords.except)]
-append!(stopwords, string.(['A':'z'; '0':'9'; '-'; '°']))
 
-# rule_numbers = string.(0:9) .=> ["０","１","２","３","４","５","６","７","８","９"]
+rule_concat = "skin disease" => "skinxdisease"
 rule_DNA = r"[ATCGU-]{4,}" => "xDNAseq"
 rule_ref = r"\[\d*\]" => ' '
 rule_numbers = string.(0:9) .=> ["O","l","Z","E","y","S","b","L","B","q"]
 rule_ascii = setdiff(['!':'@'; '[':'`'; '{':'~'; '’'], ['-']) .=> ' '
 
-files = "data/" .* readdir("data")
-txts = reduce.(*, readlines.(files))
+rule_synonym = " older " => " elder "
 
+number2 = collect(Base.product(
+    ["o","l","z","e","y","s","b","l","b","q"],
+    ["o","l","z","e","y","s","b","l","b","q"]))
+number2 = vec(prod.(number2))
+
+cd("//155.230.155.221/ty/Elderly AD/")
+# cd("G:/Elderly AD/")
+
+stopwords = CSV.read("./clinical-concepts/clinical-stopwords.txt", DataFrame)[:, 1]
+append!(stopwords, string.(['A':'z'; '0':'9'; '-'; '°']))
+append!(stopwords, number2)
+unique!(stopwords)
+
+dir = "data/JAAD/"
+files = dir .* readdir(dir)
+# filter!(file -> !occursin.("melanoma", file), files)
+@time txts = reduce.(*, readlines.(files))
+
+txts = replace.(txts, rule_concat)
 txts = replace.(txts, rule_DNA)
 txts = replace.(txts, rule_ref)
 txts = replace.(txts, rule_numbers...)
 txts = replace.(txts, rule_ascii...)
 
-@time txts = [stem_all(Stemmer("english"), txt) for txt in txts] # 8.740254 seconds
+@time txts = [stem_all(Stemmer("english"), txt) for txt in txts]
+txts = replace.(txts, rule_synonym)
 
 crps = txts .|> StringDocument |> Corpus
-@time update_lexicon!(crps)                   # 8.698965 seconds (41.99 M allocations: 2.263 GiB, 4.27% gc time)
+remove_case!(crps)
+@time update_lexicon!(crps)                  
 
 [delete!(crps.lexicon, stopword) for stopword in stopwords];
-voca = crps |> lexicon |> keys |> collect |> sort
+
+_voca = crps.lexicon |> keys |> collect
+frq1 = _voca[(crps.lexicon |> values |> collect) .≤ 5]
+[delete!(crps.lexicon, trash) for trash in frq1];
+voca = crps.lexicon |> keys |> collect |> sort
 # CSV.write("voca.csv", DataFrame(; voca), bom = true)
 
-@time spX = crps |> DocumentTermMatrix |> dtm # 8.528082 seconds (42.46 M allocations: 2.295 GiB, 3.32% gc time, 0.09% compilation time)
-@time adjM = spX'spX                          # 9.169203 seconds (19 allocations: 12.511 GiB, 0.07% gc time)
+@time spX = crps |> DocumentTermMatrix |> dtm
+@time adjM = spX'spX                         
 # spX uses only 1.35% of the space
 
 degree = sum(adjM, dims = 2) |> vec
-histogram(log10.(degree), legend = :none, xlabel = "log10(degree)", ylabel = "frequency", title = "Degree Distribution of Corpus Network")
+# histogram(log10.(degree), legend = :none, xlabel = "log10(degree)", ylabel = "frequency", title = "Degree Distribution of Corpus Network")
 
-vocafreq = DataFrame(; voca, degree)
-sort!(vocafreq, :degree, rev = true)
-CSV.write("vocafreq.csv", vocafreq, bom = true)
+vocadegree = DataFrame(; voca, degree)
+sort!(vocadegree, :degree, rev = true)
+CSV.write("vocadegree.csv", vocadegree, bom = true)
 
+atopynetwork = WordNetwork(adjM, voca)
+jldsave("atopynetwork_cached.jld2"; atopynetwork)
+cp("atopynetwork_cached.jld2", "G:/atopynetwork_cached.jld2")
 
-idx_atop = findfirst(voca .== "atop")
-atop = DataFrame(voc = voca[adjM[idx_atop, :] .> 0], weight = adjM[idx_atop, adjM[idx_atop, :] .> 0])
-sort!(atop, :weight, rev = true)
+# for node in ["age", "psoriasi", "skin", "eczema", "ad", "elder", "older", "skinxdiseas"]
+#     plot_subgraph(AN, node, 5, 2); png(node)
+#     CSV.write("$node.csv", neighbor(AN, node), bom = true)
+# end
+# neighbor(AN, "skinxdiseas")
+# plot_subgraph(AN, "skinxdiseas", 5, 2); png("skinxdiseas")
+# neighbor(AN, "skin")
+# CSV.write("AD.csv", neighbor(AN, "AD"))
+# CSV.write("eczema.csv", neighbor(AN, "eczema"))
+# CSV.write("elder.csv", neighbor(AN, "elder"))
+# CSV.write("older.csv", neighbor(AN, "older"))
 
-idx_IL_13 = findfirst(voca .== "IL-lE")
-IL_13 = DataFrame(voc = voca[adjM[idx_IL_13, :] .> 0], weight = adjM[idx_IL_13, adjM[idx_IL_13, :] .> 0])
-sort!(IL_13, :weight, rev = true)
+["age", "psoriasi", "skin", "eczema", "ad", "elder", "skinxdiseas"]
