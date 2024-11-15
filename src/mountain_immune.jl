32637fad5cbad2f3627e96f65fe3a425d826b140
dsryu0822 <rmsmsgood@naver.com>
2024-11-15 19:08:19 +0900

    thresholding \ge → > and pts log range

--- src/mountain_immune.jl
+++ src/mountain_immune.jl
@@ -10,9 +10,9 @@ device = gethostname()
 
 @load "data_immune/cached_data.jld2"
 
-wds = parse(Int64, last(device)):2:15
+wds = parse(Int64, last(device)):2:10
 folds = 1:5
-pts = 90:1:100
+pts = 1 .- logrange(0.001, .1, 10)
 
 N = length(txts)
 result = DataFrame(fd = Int64[], wd = Int64[], pt = Float64[], cp = Float64[], es = Float64[], cs1 = Float64[], cs2 = Float64[])
@@ -42,18 +42,19 @@ for wd = wds
     for fold = folds
         idx_v = fold:5:N; idx_t = setdiff(1:N, idx_v)
         for ptk = eachindex(pts)
-            adjM_t = wgtM_t_[fold] .≥ θt[fold, ptk]
+            adjM_t = wgtM_t_[fold] .> θt[fold, ptk]
 
             proper, dA, vol = zeros(N), zeros(N), zeros(N)
             @threads for iv = idx_v
                 wgtM_v = W_[iv]
-                adjM_v = wgtM_v .≥ θv[iv, ptk]
+                adjM_v = wgtM_v .> θv[iv, ptk]
                 proper[iv] = adjM_t != adjM_v
                 dA[iv] = count(adjM_t - adjM_v .< 0)
                 vol[iv] = count(adjM_v)/2
             end
             proper, dA, vol = proper[idx_v], dA[idx_v], vol[idx_v]
             cp = sum(proper .* iszero.(dA)) / length(dA)
+            cp = count(iszero.(dA)) / length(dA)
             es = sum(vol) / length(vol)
             cs1 = cp * es
             cs2 = (vol'iszero.(dA)) / length(dA)
